a8f26c356564d7cf27acdebbbccc2882a3186a70
gelbiasa <gelbifirmansyah12@gmail.com>
2025-01-11 22:44:30 +0700

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="card-tools"></div>
     </div>
     <div class="card-body">
-        Selamat datang Pengguna di Aplikasi SImudahCV yang akan mempermudah pembuatan CV anda, dimana disini Anda pembuatan CV gartis dimana anda dapat membuat CV secara gartis dan mudah dengan menggunakan template yang sudah disediakan dan anda bisa melakukan pendowloadan
+        --  Selamat datang Pengguna di Aplikasi SImudahCV yang akan mempermudah pembuatan CV anda, dimana disini Anda pembuatan CV gartis dimana anda dapat membuat CV secara gartis dan mudah dengan menggunakan template yang sudah disediakan dan anda bisa melakukan pendowloadan --
     </div>
 </div>
 
